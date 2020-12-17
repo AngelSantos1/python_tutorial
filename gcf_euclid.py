0e509abf8db4de91885413d0a5e9a05aece2dde2
Angel Santos <angelsantoscd@gmail.com>
2020-12-17 16:03:39 -0500

    Defined Function for GCF with Euclid Algorithm

---
+++ gcf_euclid.py
@@ -0,0 +1,12 @@
+#Greatest Common Factor
+#Design a function "gcf(x, y)" that returns the greatest common factor 
+# between x and y using the euclidean algorithm.
+
+def gcf(x,y):
+	while(y):
+		x, y = y, x % y
+	return x
+
+gcf = gcf(300, 400)
+print('The GCF is', gcf)
+
